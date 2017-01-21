1a4952d2e6c36e9f6dcc8c58eefdce0b78fe6d2c
Clément Roux <mileufka@gmail.com>
2017-01-21 22:07:54 +0100

    Completion paquet ajax_interaction.
    Ajout des classes MappyManager et PJManager.
    Completion de AjaxHandler.
    Installation de servlet-api.jar (Tomcat).

--- src/ajax_interaction/AjaxHandler.java
+++ src/ajax_interaction/AjaxHandler.java
@@ -1,5 +1,31 @@
 package ajax_interaction;
 
-public class AjaxHandler{
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjaxHandler extends HttpServlet implements Servlet{
 	
+	//Constructeur
+	public AjaxHandler(){
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		String resultat = "invalide";
+		String valeur = request.getParameter("valeur");
+	 
+		response.setContentType("text/xml");
+		response.setHeader("Cache-Control", "no-cache");
+	 
+		if ((valeur != null) && valeur.startsWith("X")) {
+				resultat = "valide";
+		}
+	 
+		response.getWriter().write("<message>"+resultat+"</message>");
+	}
 }
